f8b138a2dcea15d44b99e08833f3f0606fbaf52b
Khushi Donda <khushidonda02@gmail.com>
2026-05-21 17:39:30 +0530

    Add files via upload
    Initial commit for Iris flower classification project

---
+++ Iris_Flower_Classification.py
@@ -0,0 +1,13 @@
+# Import libraries
+import pandas as pd
+import numpy as np
+
+# Visualization libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# ML libraries
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.preprocessing import LabelEncoder
