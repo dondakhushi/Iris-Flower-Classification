04fdf94f430a5c1334d602900050b396398fb3ea
Khushi Donda <khushidonda02@gmail.com>
2026-05-21 12:19:00 +0000

    Initial commit for Iris flower classification project

--- Iris_Flower_Classification.py
+++ Iris_Flower_Classification.py
@@ -11,3 +11,4 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.preprocessing import LabelEncoder
+
